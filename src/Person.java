ccfb54ed3043d0b82f616f875d4946b43fbfd688
Yunus Emre Öztürk <78965275+emreoztr@users.noreply.github.com>
2021-04-09 22:39:10 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project222;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+/**
+ *
+ * @author grup15
+ */
+public abstract class Person implements Comparable<Object>{
+    private String name;
+    private String surname;
+    private UUID id;
+    private Date dateOfBirth;
+
+    public Person(String name, String surname, UUID id, Date dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", surname=" + surname + ", id=" + id + ", dateOfBirth=" + dateOfBirth + '}';
+    }
+    
+    
+    
+    
+}

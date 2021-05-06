0b46d6b5c5f237cce3c6550591e036d2c4c5ec07
elif-ay <e.ay2018@gtu.edu.tr>
2021-05-06 19:41:50 +0300

    Surgery Room
    Surgery Room class built up

--- project222/SurgeryRoom.java
+++ project222/SurgeryRoom.java
@@ -5,14 +5,27 @@
  */
 package project222;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author omera
  */
 public class SurgeryRoom extends Departments{
+	
+	private ArrayList<Doctor> doctors = new ArrayList<>();
+    private ArrayList<Nurse> nurses = new ArrayList<>();
+    private Patients patient;
+    private String surgeryName;
     
     public SurgeryRoom(int id) {
         super(id);
     }
+    
+    public public SurgeryRoom(ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patient) {
+    	this.doctors = doctors;
+    	this.nurses = nurses;
+    	this.patient = patient;
+	}
     
 }

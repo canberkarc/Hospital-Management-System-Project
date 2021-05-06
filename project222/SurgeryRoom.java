c4b3f54f065290851ebeae76537865e1c4beaa19
elif-ay <e.ay2018@gtu.edu.tr>
2021-05-06 19:57:04 +0300

    Update SurgeryRoom.java

--- project222/SurgeryRoom.java
+++ project222/SurgeryRoom.java
@@ -13,19 +13,52 @@ import java.util.ArrayList;
  */
 public class SurgeryRoom extends Departments{
 	
-	private ArrayList<Doctor> doctors = new ArrayList<>();
+    private ArrayList<Doctor> doctors = new ArrayList<>();
     private ArrayList<Nurse> nurses = new ArrayList<>();
     private Patients patient;
     private String surgeryName;
+    
+    public ArrayList<Doctor> getDoctors() {
+        return doctors;
+    }
+
+    public void setDoctors(ArrayList<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public ArrayList<Nurse> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(ArrayList<Nurse> nurses) {
+        this.nurses = nurses;
+    }
+
+    public Patients getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patients patient) {
+        this.patient = patient;
+    }
+
+    public String getSurgeryName() {
+        return surgeryName;
+    }
+
+    public void setSurgeryName(String surgeryName) {
+        this.surgeryName = surgeryName;
+    }
     
     public SurgeryRoom(int id) {
         super(id);
     }
     
-    public public SurgeryRoom(ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patient) {
+    public public SurgeryRoom(ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patient, String surgeryName) {
     	this.doctors = doctors;
     	this.nurses = nurses;
-    	this.patient = patient;
-	}
+        this.patient = patient;
+        this.surgeryName = surgeryName;
+    }
     
 }

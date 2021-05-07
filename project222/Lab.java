b0fbf1c75e40ae0e1eb2dd89902cfbd4e7ecacaf
GAFF666 <70976462+GAFF666@users.noreply.github.com>
2021-05-07 12:19:25 +0300

    Add files via upload

--- project222/Lab.java
+++ project222/Lab.java
@@ -1,18 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package project222;
+public class Lab extends Departments
+{
+	private ArrayList<LabPersonnel> labWorkers;
+	private Queue<Patient> patientsToTest;
+	private Stack<Test> testHistory;
 
-/**
- *
- * @author 
- */
-public class Lab extends Departments{
-    
-    public Lab(int id) {
-        super(id);
-    }
-    
+	public void addPatientToQueue(Patient p)
+	{
+	}
+
+	public void addLabPersonnel(LabPersonnel l)
+	{
+	}
+
+	public void addTest(Test t)
+	{
+	}
+
+	Stack<Test> getTests()
+	{
+	}
+
+	LabPersonnel getLabPersonnel(String id)
+	{
+	}
+
 }
---
+++ project222/LabPersonnel.java
@@ -0,0 +1,31 @@
+public class Personnel extends HospitalWorkers
+{
+	public Test makeBlodTest()
+	{
+	}
+
+	public Test makeCovidTest()
+	{
+	}
+
+	public Test makeUrinalTest()
+	{
+	}
+
+	public void seePatientQueue()
+	{	
+	}
+
+	public void addTestToPatient()
+	{
+	}
+
+	public void addTestToHistory()
+	{
+	}
+
+	public void seeTests()
+	{
+	}
+
+}

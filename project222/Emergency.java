fae4dc8162a3d0027f617f84595ffd93fbd0e244
Canberk Arıcı <45467111+canberkarc@users.noreply.github.com>
2021-05-07 23:19:24 +0300

    emergency upload

--- project222/Emergency.java
+++ project222/Emergency.java
@@ -1,18 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project222;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
- *
- * @author omera
- */
+* @author Canberk Arıcı
+*/
+
 public class Emergency extends Departments{
-    
-    public Emergency(int id) {
-        super(id);
+	private ArrayList<Doctor> doctors = new ArrayList<>();
+    private ArrayList<Nurse> nurses = new ArrayList<>();
+ 	private ArrayList<Patients> patients = new ArrayList<>();   
+
+ 	public ArrayList<Doctor> getDoctors() {
+        return doctors;
     }
-    
+
+    public void setDoctors(ArrayList<Doctor> doctors) {
+        this.doctors = doctors;
+    }
+
+    public ArrayList<Nurse> getNurses() {
+        return nurses;
+    }
+
+    public void setNurses(ArrayList<Nurse> nurses) {
+        this.nurses = nurses;
+    }
+
+    public ArrayList<Patients> getPatients() {
+        return patients;
+    }
+
+    public void setPatients(ArrayList<Patients> patients) {
+        this.patients = patients;
+    }
+
+    public Emergency(int id, ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patients, String surgeryName, Date surgeryDate) {
+    	this.doctors = doctors;
+    	this.nurses = nurses;
+        this.patients = patients;
+    }
+
+    public boolean intervention() {
+        // returns true if surgery completed successfully.
+    } 
 }

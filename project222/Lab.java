672e1b2f7740a99f3037546ab8970a5139d47148
Canberk Arıcı <45467111+canberkarc@users.noreply.github.com>
2021-05-31 01:42:04 +0300

    Update Lab.java

--- project222/Lab.java
+++ project222/Lab.java
@@ -1,14 +1,9 @@
 public class Lab extends Departments
 {
-	private final String name = "Lab";
 	private ArrayList<LabPersonnel> labWorkers;
 	private Queue<Patient> patientsToTest;
 	private Stack<Test> testHistory;
 	
-	public String getName(){
-		return this.name; 
-	}
-
 	public void addPatientToQueue(Patient p)
 	{
 	}

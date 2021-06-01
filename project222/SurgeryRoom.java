5f3ced1b35cb4ab9806c0304d591c72ed660b04e
elif-ay <e.ay2018@gtu.edu.tr>
2021-06-02 00:45:03 +0300

    SurgeryRoom.java updated
    SurgeryRoom.java updated

--- project222/SurgeryRoom.java
+++ project222/SurgeryRoom.java
@@ -7,19 +7,24 @@ package project222;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
+import java.util.Calendar;
 
 /**
  *
  * @author omera
  */
 public class SurgeryRoom extends Departments{
-	
+    private final String name = "Surgery Room";
     private ArrayList<Doctor> doctors = new ArrayList<>();
     private ArrayList<Nurse> nurses = new ArrayList<>();
     private Patients patient;
     private String surgeryName;
     Date surgeryDate;
     
+    public String getName() {
+        return this.name;
+    }
     public ArrayList<Doctor> getDoctors() {
         return doctors;
     }
@@ -51,12 +56,16 @@ public class SurgeryRoom extends Departments{
     public void setSurgeryName(String surgeryName) {
         this.surgeryName = surgeryName;
     }
-    
-    public SurgeryRoom(int id) {
-        super(id);
+ 
+    public Date getDate() {
+        return surgeryDate;
     }
     
-    public SurgeryRoom(int id, ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patient, String surgeryName, Date surgeryDate) {
+    public void setDate(Date surgeryDate) {
+        this.surgeryDate = surgeryDate;
+    }
+    
+    public SurgeryRoom(int id, String surgeryName, ArrayList<Doctor> doctors, ArrayList<Nurse> nurses, Patients patient, Date surgeryDate) {
         super(id);
     	this.doctors = doctors;
     	this.nurses = nurses;
@@ -66,6 +75,56 @@ public class SurgeryRoom extends Departments{
     }
 
     public boolean doSurgery() {
-        // returns true if surgery completed successfully.
+        Date currentTime = Calendar.getInstance().getTime();
+        if(currentTime.compareTo(this.getDate()) > 0) // surgery is completed
+            return true;
+        else
+            return false;
     } 
+ 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof SurgeryRoom))
+            return false;
+        SurgeryRoom s = (SurgeryRoom) o;
+        return getName().equals(s.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+    
+    @Override
+    public String toString() {
+        String result = "";
+        
+        result += "Surgery Room{ " + 
+                "\n surgery name = " + getSurgeryName() +
+                "\n surgery date = " + this.getSurgeryName() +
+                "\n patient name = " + getPatient().getName();
+        
+        if(this.getDoctors().size() > 0) {
+            result += "\n doctors = {";
+            for(int i = 0; i < this.getDoctors().size(); ++i) {
+                result += this.getDoctors().get(i).getName() + " " +
+                        this.getDoctors().get(i).getSurname() + ", ";
+            }
+            result += "}";
+        }
+        
+        if(this.getNurses().size() > 0) {
+            result += "\n nurses = {";
+            for(int i = 0; i < this.getNurses().size(); ++i) {
+                result += this.getNurses().get(i).getName() + " " +
+                        this.getNurses().get(i).getSurname() + ", ";
+            }
+            result += "} \n";
+        }
+        result += "} \n";
+        
+        return result;
+    }
 }
